19c81a286dced73d7b181f4fa969c55e17c20661
sander-atten-galileo <sander.atten@galileo-academy.nl>
2024-01-23 15:49:40 +0100

    Added game logic
    Added the game logic created with DartPad to the project - not functional yet!

---
+++ lib/game_logic.dart
@@ -0,0 +1,85 @@
+import 'dart:math';
+
+class Codepin {
+  int colorValue;
+  Codepin(this.colorValue);
+  String get colorOfPin {
+    switch (colorValue) {
+      case 0:
+        return 'No color';
+      case 1:
+        return 'Rood';
+      case 2:
+        return 'Blue';
+      case 3:
+        return 'Green';
+      case 4:
+        return 'Yellow';
+      case 5:
+        return 'Purple';
+      case 6:
+        return 'Orange';
+      default:
+        return 'Error';
+    }
+  }
+}
+
+class Scorepin {
+  int colorValue;
+  Scorepin(this.colorValue);
+  String get colorOfPin {
+    switch (colorValue) {
+      case 0:
+        return 'No color';
+      case 1:
+        return 'White';
+      case 2:
+        return 'Black';
+      default:
+        return 'Error';
+    }
+  }
+}
+
+// Initialize empty list
+List<int> secretCode = [];
+
+// Function that creates and returns a list of 4 unique integers between 1 and 6
+List<int> createNewCodeSnippet() {
+  List<int> newCodeSnippet = [];
+  while (newCodeSnippet.length < 4) {
+    int nextValue = Random().nextInt(6) + 1;
+    if (newCodeSnippet.contains(nextValue) == false) {
+      newCodeSnippet.add(nextValue);
+    }
+
+  }
+  return newCodeSnippet;
+}
+
+// Function that checks a list against the list stored in secretCode and returns a list with feedback
+List<int> checkCodes(inputCode) {
+  List<int> controlFeedback = [];
+  for (int i = 0; i < 4; i++) {
+    if (inputCode[i] == secretCode[i]) {
+      controlFeedback.add(2);
+    } else if (secretCode.contains(inputCode[i])) {
+      controlFeedback.add(1);
+    } else {
+      controlFeedback.add(0);
+    }
+  }
+  return controlFeedback;
+}
+
+void runGame() {
+  secretCode = createNewCodeSnippet();
+  print(secretCode);
+
+  List<int> myCodeSnippet = [1, 2, 3, 4];
+  print(checkCodes(myCodeSnippet));
+
+  print(Codepin(3).colorOfPin);
+  print(Scorepin(2).colorOfPin);
+}
